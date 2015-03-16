c750966bae372f60baa59bd5afd46e1c0e82c28a
Chris Haynes (mac) <chris.haynes@gmail.com>
2015-03-16 22:45:53 +0000

    refactor view event listeners

--- public/js/app.js
+++ public/js/app.js
@@ -87,68 +87,6 @@ if (Offline.isOnline()) {
   }
 }
 
-/*
-let overlay = document.querySelector('.overlay');
-let overlayContent = overlay.querySelector('.overlay__content__body');
-let overlayTitle = overlay.querySelector('.overlay__title');
-
-
-function overlayOpen(title) {
-  overlayTitle.textContent = title;
-  overlay.classList.add('overlay--show');
-}
-
-function overlayClose() {
-  overlay.classList.remove('overlay--show');
-}
-
-overlay.addEventListener('click', event => {
-  if (event.target.classList.contains('overlay__close') ||
-      event.target.parentNode.classList.contains('overlay__close') ) {
-      overlayClose();
-  }
-});
-
-function chooseSelectMe(el, select, value, text) {
-  el.textContent = text;
-  select.value = value;
-  overlayClose();
-}
-
-function openSelectMe(el) {
-  let form = el.form;
-  let connect = el.dataset.connect;
-  let select = form.querySelector('select[name=' + connect + ']');
-  let currentValue = select.value;
-  let numOptions = select.options.length;
-
-  let optionText = '';
-
-  optionText += `<div class='selectmebox'><ul class='selectmebox__inner'>`;
-  for(let i=0; i<numOptions; i++) {
-    let selected = '';
-    if (select.options[i].value === currentValue) {
-      selected = 'selectmebox__inner__option--selected';
-    }
-    optionText += `
-      <li class='selectmebox__inner__option ${selected} btn' data-value='${select.options[i].value}' data-name='${select.options[i].text}'>${select.options[i].text}</li>
-    `;
-  }
-  optionText += `</ul></div>`;
-
-  overlayContent.innerHTML = optionText;
-
-  let options = overlayContent.querySelectorAll('.selectmebox__inner__option');
-  for (let i=0; i<options.length; i++) {
-    options[i].addEventListener('click', event => {
-      chooseSelectMe(el, select, event.target.dataset.value, event.target.dataset.name);
-    });
-  }
-
-  overlayOpen('Select job');
-}
-*/
-
 document.body.addEventListener('click', (event) => {
   if (typeof event.target.dataset.nav !== 'undefined') {
     event.preventDefault();
